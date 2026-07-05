b25dba07b661d0c161d76f71d4f1109381e19db3
FFalquiCao <f.falquicao@studenti.unipi.it>
2026-07-05 22:48:29 +0000

    Update soluzione.cpp
    La soluzione aggiunge un passaggio all'algoritmo del Union-Find: il rappresentante di ciascun blocco punta sempre ad un nodo di valore massimale, e quando due insiemi vengono uniti i loro nodi massimali vengono messi in relazione (il più grande diventa il parent del più piccolo).

--- Codice/soluzione.cpp
+++ Codice/soluzione.cpp
@@ -5,7 +5,7 @@ const int LINES = 594924;
 //const int LINES = 3;
 
 
-vector<int> parent, value, dimen, ins;
+vector<int> parent, value, dimen, ins, root;
 
 void make_set(int v) {
     ins[v] = v;
@@ -15,7 +15,7 @@ void make_set(int v) {
 int find_set(int v) {
     if (v == ins[v])
         return v;
-    return v = find_set(ins[v]);
+    return ins[v] = find_set(ins[v]);
 }
 
 void union_sets(int a, int b) {
@@ -26,6 +26,15 @@ void union_sets(int a, int b) {
             swap(a, b);
         ins[b] = a;
         dimen[a] += dimen[b];
+
+        //ogni elemento terminale del find_set deve puntare ad un elemento di valore max nel suo sottoinsieme
+        //inoltre ogni insieme che inizia a puntare su un altro deve aggiornare l'albero parent
+        if(value[root[a]] < value[root[b]]) {
+            parent[root[a]] = root[b];
+            root[a] = root[b];
+        } else {
+            parent[root[b]] = root[a];
+        }
     }
 }
 
@@ -52,11 +61,15 @@ int main() {
     ins = vector<int> (2*n, 0);
     dimen = vector<int> (2*n, 1);
     parent = vector<int> (2*n);
+    root = vector<int> (2*n);
 
     for(int i = 0; i<2*n; i++) {
         ins[i] = i;
         parent[i] = i;
+        root[i] = i;
     }
+
+    int newNode = n;
 
     for(int i = 0; i<m; i++) {
         int w, ln, gn; cin >> w >> ln >> gn;
@@ -70,26 +83,31 @@ int main() {
 
         int a = find_set(ln);
         int b = find_set(gn);
+
         
         if(a == b) continue;
-        union_sets(a,b);
+        if(value[root[a]] < w && value[root[a]] < w) {
+            // si crea una nuova componente connessa
 
+            value[newNode] = w;
+            union_sets(a,newNode);
+            union_sets(b,newNode);
+            newNode++;
+        } else {
+            // non si crea una nuova componente connessa
+            union_sets(a,b);
+        } 
     }
 
-    for(int i = 0; i<20; i++) {
-        cout << find_set(30*i) << " " << dimen[30*i] << "\n";
-    }
+    int j = find_set(0); 
 
+    cout << root[j] << " " << value[root[j]];
 
-
-
-
+    
     //int a, b; cin >> a >> b; //nodi di cui trovare il minmax path
     //a = NodiregToComp[a]; b = NodiregToComp[b]; 
-
-    
-    
-    
     //cout << r;
     
+
+    // 234 266 hanno percorso minimo 48776. Questo perché 234 si connette esclusivamente a 266, ma lo fa molto spesso.
 }
