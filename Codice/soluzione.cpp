346990a4a2e582caf93cdc26bf240215e19644ff
FFalquiCao <f.falquicao@studenti.unipi.it>
2026-07-05 20:23:12 +0000

    Update soluzione.cpp
    Primi passi per la soluzione 2, implementazione dello Union-Find.

--- Codice/soluzione.cpp
+++ Codice/soluzione.cpp
@@ -4,6 +4,32 @@ using namespace std;
 const int LINES = 594924;
 //const int LINES = 3;
 
+
+vector<int> parent, value, dimen, ins;
+
+void make_set(int v) {
+    ins[v] = v;
+    dimen[v] = 1;
+}
+
+int find_set(int v) {
+    if (v == ins[v])
+        return v;
+    return v = find_set(ins[v]);
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (dimen[a] < dimen[b])
+            swap(a, b);
+        ins[b] = a;
+        dimen[a] += dimen[b];
+    }
+}
+
+
 int main() {
 
     int n; cin >> n; // numero di nodi
@@ -21,52 +47,49 @@ int main() {
     int V; cin >> V; // valore del peso max: tutti i pesi w sono interi fra 1 e V
     
     int m; cin >> m; //numero di archi
-    vector<vector<pair<int,int>>> adj(n, vector<pair<int,int>>(0));
+
+    value = vector<int> (2*n, 0);
+    ins = vector<int> (2*n, 0);
+    dimen = vector<int> (2*n, 1);
+    parent = vector<int> (2*n);
+
+    for(int i = 0; i<2*n; i++) {
+        ins[i] = i;
+        parent[i] = i;
+    }
+
     for(int i = 0; i<m; i++) {
         int w, ln, gn; cin >> w >> ln >> gn;
         ln = NodiregToComp[ln];
         gn = NodiregToComp[gn];
 
-        if(gn >= n || gn >= n) {
+        if(gn >= n || ln >= n) {
             cerr << "Huh";
             return 0;
         }
-        adj[ln].push_back(make_pair(gn, w));
-        adj[gn].push_back(make_pair(ln, w));
+
+        int a = find_set(ln);
+        int b = find_set(gn);
+        
+        if(a == b) continue;
+        union_sets(a,b);
+
     }
 
-    //for(auto u : adj[0]) {
-    //    cout << u.first << " " << u.second << "\n";
-    //}
+    for(int i = 0; i<20; i++) {
+        cout << find_set(30*i) << " " << dimen[30*i] << "\n";
+    }
 
-    int a, b; cin >> a >> b; //nodi di cui trovare il minmax path
+
+
+
+
+    //int a, b; cin >> a >> b; //nodi di cui trovare il minmax path
     //a = NodiregToComp[a]; b = NodiregToComp[b]; 
 
     
-    vector<bool> visited(n);
-    auto dfs = [&](int index, auto dfs, int w) {
-        if(visited[index]) return;
-        visited[index] = true; 
-
-        for(auto u : adj[index]) {
-            if(u.second <= w)
-                dfs(u.first, dfs, w);
-        }
-    };
-
-    //cerchiamo il minimo v per cui visited[b]=true 
-    int l = 1; int r = V;
-    int mid;
-    while(l != r) {
-        mid = (l + r) / 2;
-        visited = vector<bool>(n, false);
-        dfs(a, dfs, mid);
-        if (visited[b])
-            r = mid;
-        else 
-            l = mid + 1;
-   }
     
-    cout << r;
+    
+    //cout << r;
     
 }
