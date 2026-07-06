98a73bd7f78203521383a34e10a4c0d5ddf639f7
FFalquiCao <f.falquicao@studenti.unipi.it>
2026-07-06 00:22:13 +0000

    Update soluzione.cpp
    Inizio development Union-Find. Il resto del codice è buggato, serve maneggiarci

--- Codice/soluzione.cpp
+++ Codice/soluzione.cpp
@@ -1,11 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int LINES = 594924;
-//const int LINES = 3;
 
 
 vector<int> parent, value, dimen, ins, root;
+vector<vector<int>> adj;
+
+const int lgmax = 20;
+vector<int> level;
+vector<vector<int>> anc(lgmax);
 
 void make_set(int v) {
     ins[v] = v;
@@ -28,16 +31,36 @@ void union_sets(int a, int b) {
         dimen[a] += dimen[b];
 
         //ogni elemento terminale del find_set deve puntare ad un elemento di valore max nel suo sottoinsieme
-        //inoltre ogni insieme che inizia a puntare su un altro deve aggiornare l'albero parent
+        //inoltre ogni coppia di insiemi che si uniscono sono connessi nell'albero
         if(value[root[a]] < value[root[b]]) {
             parent[root[a]] = root[b];
             root[a] = root[b];
         } else {
             parent[root[b]] = root[a];
         }
+        adj[root[a]].push_back(root[b]);
+        adj[root[b]].push_back(root[a]);
     }
 }
 
+int getLca(int x, int y) {
+
+    if(level[x] < level[y]) swap(x, y);
+
+    for(int h = lgmax; 0 <= h; h--) {
+        if(level[y] + (1 << h) <= level[x])
+            x = anc[h][x];
+    }
+    if(x == y) return x;
+
+    for(int h = lgmax; 0 <= h; h--) {
+        if(anc[h][x] != anc[h][y]) {
+            x = anc[h][x];
+            y = anc[h][y];
+        }
+    }
+    return anc[0][x];
+}
 
 int main() {
 
@@ -62,6 +85,7 @@ int main() {
     dimen = vector<int> (2*n, 1);
     parent = vector<int> (2*n);
     root = vector<int> (2*n);
+    adj.resize(2*n);
 
     for(int i = 0; i<2*n; i++) {
         ins[i] = i;
@@ -70,7 +94,7 @@ int main() {
     }
 
     int newNode = n;
-
+    
     for(int i = 0; i<m; i++) {
         int w, ln, gn; cin >> w >> ln >> gn;
         ln = NodiregToComp[ln];
@@ -84,11 +108,9 @@ int main() {
         int a = find_set(ln);
         int b = find_set(gn);
 
-        
         if(a == b) continue;
         if(value[root[a]] < w && value[root[a]] < w) {
             // si crea una nuova componente connessa
-
             value[newNode] = w;
             union_sets(a,newNode);
             union_sets(b,newNode);
@@ -99,15 +121,34 @@ int main() {
         } 
     }
 
-    int j = find_set(0); 
+    //anc = vector<vector<int>> (lgmax, vector<int>(2*n));
 
-    cout << root[j] << " " << value[root[j]];
+    //for(int i = 0; i<2*n; i++) {
+    //    anc[0][i] = parent[i];
+    //}
 
-    
+
+    int sum = 0;
+    for(int i = 0; i<2*n; i++) {
+        sum+=adj[i].size();
+    }
+    cout << sum << " " << 2*newNode;
+    /*vector<bool> visited(2*n);
+    auto dfs = [&](int index, auto dfs, int depth) {
+        if(visited[index]) return;
+        visited[index] = true; 
+        level[index] = depth;
+
+        for(auto u : adj[index]) dfs(u, dfs, depth+1);
+    };
+    dfs(newNode-1, dfs, 1);
+
+    for(int i = 0; i<100; i++) cout << level[i] << "\n";
     //int a, b; cin >> a >> b; //nodi di cui trovare il minmax path
     //a = NodiregToComp[a]; b = NodiregToComp[b]; 
     //cout << r;
     
 
-    // 234 266 hanno percorso minimo 48776. Questo perché 234 si connette esclusivamente a 266, ma lo fa molto spesso.
+    // 234 266 hanno percorso minimo 48776. Questo perché 234 si connette esclusivamente a 266, ma lo fa molto spesso.*/
 }
+
